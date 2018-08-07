8d55e076740e150804659c44b1e1550a01fdbd17
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-08-07 12:32:29 +0530

    Changing browser
    Browser from PhantomJS to headless Firefox

--- cet_logn.py
+++ cet_logn.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 import time
 
-driver = webdriver.PhantomJS() # or add to your PATH
+options = Options()
+options.set_headless(headless=True)
+driver = webdriver.Firefox(firefox_options=options)
 driver.get('http://172.16.0.1:8090')
 uname = '<type-your-user-name-here>'
 password = '<type-your-password-here>'
