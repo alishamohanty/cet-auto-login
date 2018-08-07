d4d9e2529c6f28ec2ce49adfa851662b67cf7682
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-08-07 11:33:25 +0530

    Basic functionality is working i.e loggin you into the portal

---
+++ cet_logn.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.PhantomJS() # or add to your PATH
+driver.set_window_size(1024, 768) # optional
+driver.get('http://172.16.0.1:8090')
+uname = '<type-your-user-name-here>'
+password = '<type-your-password-here>'
+wait = WebDriverWait(driver, 10)
+#Username
+time.sleep(3)	
+user = driver.find_element_by_class_name('textbox')
+user.clear()
+user.send_keys(uname)
+#Password
+time.sleep(3)
+user = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/form/div[1]/div[2]/div[2]/table/tbody/tr[4]/td/input")))
+user.clear()
+user.send_keys(password)
+#Login
+el = driver.find_element_by_id('logincaption')
+el.click()
+print("Login done")
