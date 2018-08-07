15a36d764fe1eb1ac020e87daf479c6648a5718a
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-08-07 14:08:20 +0530

    Logout function added

--- cet_logn.py
+++ cet_logn.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import time
+import sys
 
 options = Options()
 options.set_headless(headless=True)
@@ -12,18 +13,40 @@ driver.get('http://172.16.0.1:8090')
 uname = '<type-your-user-name-here>'
 password = '<type-your-password-here>'
 wait = WebDriverWait(driver, 10)
-#Username
-time.sleep(3)	
-user = driver.find_element_by_class_name('textbox')
-user.clear()
-user.send_keys(uname)
-#Password
-time.sleep(3)
-user = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/form/div[1]/div[2]/div[2]/table/tbody/tr[4]/td/input")))
-user.clear()
-user.send_keys(password)
+#Logout
+if sys.argv[0] == 'logout':
+	#Username
+	time.sleep(5)
+	user = driver.find_element_by_class_name('textbox')
+	user.clear()
+	user.send_keys(uname)
+	#Password
+	time.sleep(5)
+	user = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/form/div[1]/div[2]/div[2]/table/tbody/tr[4]/td/input")))
+	user.clear()
+	user.send_keys(password)
+	#Login
+	login = driver.find_element_by_id('logincaption')
+	login.click()
+	print("Login")
+	#Logout
+	logout = driver.find_element_by_id('')
+	logout.click()
+	print("Logut")
+	driver.close()
 #Login
-el = driver.find_element_by_id('logincaption')
-el.click()
-print("Login done")
-driver.close()
+else :
+  	#Username
+	time.sleep(5)
+	user = driver.find_element_by_class_name('textbox')
+	user.clear()
+	user.send_keys(uname)
+	#Password
+	time.sleep(5)
+	user = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/form/div[1]/div[2]/div[2]/table/tbody/tr[4]/td/input")))
+	user.clear()
+	user.send_keys(password)
+	#Login
+	login = driver.find_element_by_id('logincaption')
+	login.click()
+	print("Login")
