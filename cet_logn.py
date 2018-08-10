f1a74ad48f3469c950e1b6b8836aea832da77c6f
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-08-10 18:37:57 +0530

    Final script

--- cet_logn.py
+++ cet_logn.py
@@ -7,42 +7,38 @@ import time
 import sys
 
 options = Options()
-options.set_headless(headless=True)
+options.set_headless(headless=True) 
 driver = webdriver.Firefox(firefox_options=options)
 driver.get('http://172.16.0.1:8090')
-uname = '<type-your-user-name-here>'
-password = '<type-your-password-here>'
-wait = WebDriverWait(driver, 10)
-#Logout
-if sys.argv[0] == 'logout':
+uname = '<user-id-goes-here>'
+password = '<password-goes-here>'
+wait = WebDriverWait(driver, 2)
+
+if sys.argv[1] == 'logout':
 	#Username
-	time.sleep(5)
+	time.sleep(1)
 	user = driver.find_element_by_class_name('textbox')
 	user.clear()
 	user.send_keys(uname)
 	#Password
-	time.sleep(5)
+	time.sleep(1)
 	user = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/form/div[1]/div[2]/div[2]/table/tbody/tr[4]/td/input")))
 	user.clear()
 	user.send_keys(password)
 	#Login
 	login = driver.find_element_by_id('logincaption')
 	login.click()
-	print("Login")
-	#Logout
-	logout = driver.find_element_by_id('')
-	logout.click()
+	login.click()
 	print("Logut")
 	driver.close()
-#Login
 else :
   	#Username
-	time.sleep(5)
+	time.sleep(1)
 	user = driver.find_element_by_class_name('textbox')
 	user.clear()
 	user.send_keys(uname)
 	#Password
-	time.sleep(5)
+	time.sleep(1)
 	user = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/form/div[1]/div[2]/div[2]/table/tbody/tr[4]/td/input")))
 	user.clear()
 	user.send_keys(password)
@@ -50,3 +46,4 @@ else :
 	login = driver.find_element_by_id('logincaption')
 	login.click()
 	print("Login")
+	driver.close()
